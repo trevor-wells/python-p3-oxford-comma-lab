43095aed5739b5085a8bac6e21479617e6226554
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-08 11:03:07 -0600

    solved in a very silly way

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 2 or len(items) == 1:
+        return ' and '.join(items)
+    last = items[-1]
+    joined = ', '.join(items)
+    getting_there = joined.split(f'{last}')
+    almost = 'and '.join(getting_there)
+    return almost + last
